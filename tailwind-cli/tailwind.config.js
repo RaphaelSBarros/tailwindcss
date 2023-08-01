92ed172b38252b55a4c4f5d66f5cf14f411a9a9a
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2023-08-01 19:48:32 -0300

    Aprendendo a instalar o tailwind em diferentes contextos

---
+++ tailwind-cli/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts,js}" //monitoramento das extensões
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
